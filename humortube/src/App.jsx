c747d578451405ae06e30c904e9281c57edf051e
LEE-JUN-KYOUNG <dkwlswk8778@gmail.com>
2025-08-07 16:38:57 +0900

    유머튜브 완성

---
+++ humortube/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+    <h1>웃긴 유튜브 영상 모음</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-1P7XNCQ4zM?si=gcV1IQBsvrBBv8D5&amp;start=22" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/qf-TNExCCUQ?si=jH1nDX2O0bKMFPSD&amp;start=24" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+    </>
+ )
+}
+
+export default App
